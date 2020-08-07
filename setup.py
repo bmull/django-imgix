26c928087e7b890f49817966c11d8ec64bdb9d83
Brenden Mulligan <b@bmull.com>
2020-08-06 17:22:48 -0700

    Replace django.utils.six with six, increase django and imgix versions

--- setup.py
+++ setup.py
@@ -20,8 +20,9 @@ setup(
     author='Pancentric Ltd',
     author_email='devops@pancentric.com',
     install_requires=[
-        'django<2.0.0',
-        'imgix<2.0.0',
+        'django<3.1.0',
+        'imgix<3.2.1',
+        'six<1.15.0',
     ],
     classifiers=[
         'Environment :: Web Environment',
